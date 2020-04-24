df0e3f1478237a2ed5b5067df54637f38ffa0fc7
NiekSulter <nieksulter99@gmail.com>
2020-04-24 11:57:03 +0200

    24-4: disabled caching

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ from Bio.Alphabet import generic_dna
 
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 @app.route('/')
 def hello_world():
@@ -21,3 +33,4 @@ def convert():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
